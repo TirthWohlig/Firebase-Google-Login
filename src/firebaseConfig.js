070cb4cbb611368e9893c8ebea9b71f29ecdd1d4
TirthWohlig <tirthwohlig@gmail.com>
2023-03-09 14:13:30 +0530

    First Commit

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyBKig4PzDD-74K23DqTEpaJDbY-BTPLD-8",
+  authDomain: "invoicez-co-in.firebaseapp.com",
+  projectId: "invoicez-co-in",
+  storageBucket: "invoicez-co-in.appspot.com",
+  messagingSenderId: "304557408513",
+  appId: "1:304557408513:web:fc1b7aa5c5490edd60db00",
+  measurementId: "G-Y5BCSX87XW"
+};
